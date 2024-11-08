7263c8af9894e4283749d05c9f557c88588a808f
Aya emam <ayaemam98@gmail.com>
2024-11-08 21:12:28 +0200

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/StockSquares-Educational-Frontend/',  // Replace this with your repo name
   plugins: [react()],
 })
